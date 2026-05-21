ae2e86649fee6f4fe2c718ff18148722d9edcb53
Milad <miladallahgholi95@gmail.com>
2026-05-21 13:33:51 +0330

    change django version

--- storage/urls.py
+++ storage/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path("drive/root", RootDriveView.as_view()),
     path("drive/folders/<int:pk>", FolderContentView.as_view()),
 
-    path("folders", FolderCreateView.as_view()),
+    path("folders/create/", FolderCreateView.as_view()),
     path("folders/<int:pk>", FolderUpdateDeleteView.as_view()),
     path("folders/<int:pk>/share", FolderShareView.as_view()),
 
-    path("files", FileCreateView.as_view()),
+    path("files/upload/", FileCreateView.as_view()),
     path("files/<int:pk>", FileUpdateDeleteView.as_view()),
     path("files/<int:pk>/download", FileDownloadView.as_view()),
     path("files/<int:pk>/share", FileShareView.as_view()),
